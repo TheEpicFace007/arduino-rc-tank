625bf9aba23bd7d5d734bbb3c854b4d481dbbaea
ViniDalvino <vinidalvino@cock.li>
2021-02-25 18:03:32 -0500

    404 shit

--- src/Routes/404/404.tsx
+++ src/Routes/404/404.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./404.scss";
 
+document.title = "Page introuvable";
+
 export function UnknownPage()
 {
   return (
     <>
-      <h1>Page inconnu</h1>
+      <h1>Page introuvable</h1>
       <p><a href="/">Clique ici pour retourné au menu principale</a></p>
     </>
   );
