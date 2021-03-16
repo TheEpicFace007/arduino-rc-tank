5231d4af0963b532e8c4f2f64fbf7d98562c2ee0
ViniDalvino <vinidalvino@cock.li>
2021-03-16 19:45:21 -0400

    fait la page 404

--- front-end/src/Routes/404/404.tsx
+++ front-end/src/Routes/404/404.tsx
@@ -9,7 +9,7 @@ export function UnknownPage()
   return (
     <>
       <h1>Page introuvable</h1>
-      <p><a href="/">Clique ici pour retourné au menu principale</a></p>
+      <p><a href="/">Clique ici pour retourner au menu principale</a></p>
     </>
   );
 }
